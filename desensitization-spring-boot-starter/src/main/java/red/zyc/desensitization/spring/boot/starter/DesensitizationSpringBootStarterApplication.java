655061377cc4f767951432c6b7d1de2946e3f9fc
zyc <1343786337@qq.com>
2019-12-04 22:41:46 +0800

    Initial commit

---
+++ desensitization-spring-boot-starter/src/main/java/red/zyc/desensitization/spring/boot/starter/DesensitizationSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package red.zyc.desensitization.spring.boot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesensitizationSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesensitizationSpringBootStarterApplication.class, args);
+    }
+
+}
